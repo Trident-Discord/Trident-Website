9b5cc7f29c9624d2bebcd1aef2af1fe038528231
Damian Freeman <49081789+iheartmemphis@users.noreply.github.com>
2019-03-30 18:37:49 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000; // Default hosting provider port.
+
+app.use(express.static("public"));
+
+app.get("/", function(request, response) {
+    response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/commands", function(request, response) {
+    response.sendFile(__dirname + "/views/commands.html");
+});
+
+app.get("/hidden", function(request, response) {
+    response.sendFile(__dirname + "/views/easter.html");
+});
+
+app.get("*", function(request, response) {
+    response.status(404).send("Not Found");
+});
+
+const listener = app.listen(port, function() {
+    console.log(`Express has started on port ${port} and is now live.`);
+});
